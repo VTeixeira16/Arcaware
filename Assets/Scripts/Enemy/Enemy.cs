be7680950a79ec7eaa4110bc00b65d7e2298b4fd
Victor Teixeira <victorteixeirasp@gmail.com>
2021-05-08 23:42:31 -0300

    [ARCAWARE]Adicionado animacao de morte ao inimigo.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class Enemy : Persons_core
 {
+
+    Animator enemyAnimator;
+
+    void Start()
+    {
+        enemyAnimator = GetComponent<Animator>();
+    }
+
     void Update()
     {
+        enemyAnimator.SetInteger("hp", _hp);
+        Debug.Log("hp enemy: " + _hp);
         if (hp <= 0)
         {
             hp = 0;
-            HpZerou();
+
+            if(enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("C_Lancer_Death") &&
+               enemyAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                HpZerou();
+            }
         }
     }
 
